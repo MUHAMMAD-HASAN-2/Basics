5fb71ba84afbfeddd16e075ba3eb73b772956f1f
Hasan <mhasantrq@gmail.com>
2024-01-19 21:28:47 -0800

    My first product page

---
+++ project.js
@@ -0,0 +1,6 @@
+document.getElementById('subscribeForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // You can add your subscription logic here
+    alert('Subscription successful!');
+  });
+  
